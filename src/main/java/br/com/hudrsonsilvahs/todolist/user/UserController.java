7c8b62267e3e87a5c58a6ec8a2a3a5493d42a33e
hudrson.silva <hudrsonsilvahs@hotmail.com>
2025-02-04 15:38:50 -0300

    foi adicionando, h2, tabela usuario, repositorio.

--- src/main/java/br/com/hudrsonsilvahs/todolist/user/UserController.java
+++ src/main/java/br/com/hudrsonsilvahs/todolist/user/UserController.java
@@ -1,6 +1,7 @@
 package br.com.hudrsonsilvahs.todolist.user;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getName());;
+    @Autowired
+    private IUserRepository userRepository;
 
+
+
+    @PostMapping("/")
+    public UserModel create(@RequestBody UserModel userModel) {
+       var userCreated =  this.userRepository.save(userModel);
+       return userCreated;
     }
 
 }
--- src/main/java/br/com/hudrsonsilvahs/todolist/user/UserModel.java
+++ src/main/java/br/com/hudrsonsilvahs/todolist/user/UserModel.java
@@ -4,15 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
-@Entity(name= "tb_users")
+@Entity(name = "tb_users")
 public class UserModel {
 
     @Id
-
     @GeneratedValue(generator = "UUID")
     private UUID id;
 
@@ -20,4 +21,6 @@ public class UserModel {
     private String name;
     private String password;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
