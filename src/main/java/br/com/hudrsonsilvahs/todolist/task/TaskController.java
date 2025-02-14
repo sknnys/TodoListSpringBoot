49f749e8113db104b4fcbff48e0b20acaba6e7b1
hudrson.silva <hudrsonsilvahs@hotmail.com>
2025-02-14 11:44:10 -0300

    Validação das horas

--- src/main/java/br/com/hudrsonsilvahs/todolist/task/TaskController.java
+++ src/main/java/br/com/hudrsonsilvahs/todolist/task/TaskController.java
@@ -4,11 +4,15 @@ import br.com.hudrsonsilvahs.todolist.user.UserModel;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
@@ -19,10 +23,24 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request){
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
-        return this.taskRepository.save(taskModel);
+
+        var currentDate = LocalDateTime.now();
+
+       if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+               return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("data de inicio e de termino deve ser maior q atual");
+
+       }
+
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt()) ){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("data de inicio tem que ser menor que a de termino");
+
+        }
+
+        var task = taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 
 }
