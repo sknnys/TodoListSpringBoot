f94fd4a409d7a79bff46d192c2646f3cdf7fdc77
hudrson.silva <hudrsonsilvahs@hotmail.com>
2025-02-14 11:22:09 -0300

    Setando atributo de usuario na request.

--- src/main/java/br/com/hudrsonsilvahs/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/hudrsonsilvahs/todolist/filter/FilterTaskAuth.java
@@ -1,9 +1,11 @@
 package br.com.hudrsonsilvahs.todolist.filter;
 
-import br.com.hudrsonsilvahs.todolist.user.UserModel;
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.hudrsonsilvahs.todolist.user.IUserRepository;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,34 +15,53 @@ import java.util.Base64;
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+
+        var servletPath = request.getServletPath();
+        if(servletPath.equals("/tasks/")) {
+
             //pegar a autenticação
+            var authorization = request.getHeader("Authorization");
+            var authEncoded = authorization.substring("Basic".length()).trim();
 
-        var authorization = request.getHeader("Authorization");
-        var authEncoded = authorization.substring("Basic".length()).trim();
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded);
 
-        byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+            var authString = new String(authDecode);
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
 
-        var authString = new String(authDecode);
-        String[] credentials = authString.split(":");
-        String username = credentials[0];
-        String password = credentials[1];
-
-        System.out.println("Authorization: ");
-        System.out.println(username);
-        System.out.println(password);
+            System.out.println("Authorization: ");
+            System.out.println(username);
+            System.out.println(password);
 
             //validar usuario
+            var user = this.userRepository.findByUsername(username);
+            if (user == null) {
+                response.sendError(401);
+            }else {
+                //validar Senha
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(),user.getPassword());
+                if(passwordVerify.verified) {
+                    request.setAttribute("idUser", user.getId());
 
-
-            // validar senha
-
+                    filterChain.doFilter(request, response);
+                }else {
+                    response.sendError(401);
+                }
+            }
 
             //segue viagem
-
+            }else {
+            System.out.println("Chegou no filtro");
 
         filterChain.doFilter(request, response);
     }
+}
+
 }
--- src/main/java/br/com/hudrsonsilvahs/todolist/task/TaskController.java
+++ src/main/java/br/com/hudrsonsilvahs/todolist/task/TaskController.java
@@ -1,10 +1,15 @@
 package br.com.hudrsonsilvahs.todolist.task;
 
+import br.com.hudrsonsilvahs.todolist.user.UserModel;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,8 +19,9 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel){
-        System.out.println("Chegou no controller");
+    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
         return this.taskRepository.save(taskModel);
     }
 
